3b1c4bf2785b6609d77e46fb13bc23740a34e270
khader12345 <147363653+khader12345@users.noreply.github.com>
2024-04-06 05:16:25 -0400

    Properties of the class MeetingRoom created

--- Assignment3/BusinessLogic/MeetingRoom.cs
+++ Assignment3/BusinessLogic/MeetingRoom.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace Assignment3.BusinessLogic
 {
-    internal class MeetingRoom
+    public class MeetingRoom
     {
+
+        private string _roomNumber;
+
+        private int _seatingCapacity;
+
+        public string RoomNumber
+        {
+            get { return _roomNumber; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("A room number must be provided");
+                }
+                _roomNumber = value;
+            }
+        }
+
+        public int SeatingCapacity
+        {
+            get { return _seatingCapacity; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Seating capacity should be more than 0");
+                }
+                _seatingCapacity = value;
+            }
+        }
+        public RoomLayoutType RoomLayoutType { get; set; }
+
+        public string RoomImageFile => $"{RoomLayoutType}".ToLower() + "_img.png";
+        public string RoomTypeIcon
+        {
+            get { return $"{RoomLayoutType.ToString().ToLower()}_icon.png"; }
+        }
+
+        public string DisplayText
+        {
+            get { return $"Layout: {RoomLayoutType}, Capacity: {SeatingCapacity}"; }
+        }
+
+        public MeetingRoom(string roomNumber, int seatingCapacity, RoomLayoutType roomLayoutType)
+        {
+            RoomNumber = roomNumber;
+            SeatingCapacity = seatingCapacity;
+            RoomLayoutType = roomLayoutType;
+        }
     }
 }
