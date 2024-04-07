cfea9c4605801bad7a33ab26d721ef8233a7384d
khader12345 <147363653+khader12345@users.noreply.github.com>
2024-04-07 16:08:50 -0400

    PickRoomPage Created along with its attributes

--- Assignment3/Pages/PickRoomPage.xaml.cs
+++ Assignment3/Pages/PickRoomPage.xaml.cs
@@ -1,9 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assignment3.BusinessLogic;
+
+
+
 namespace Assignment3.Pages;
 
 public partial class PickRoomPage : ContentPage
 {
-	public PickRoomPage()
-	{
-		InitializeComponent();
-	}
+    private ReservationRequestManager _reservationRequestManager = new ReservationRequestManager();
+    private MeetingRoom selectedRoom;
+
+    public PickRoomPage()
+    {
+        InitializeComponent();
+        _reservationRequestManager.InitializeMeetingRooms();
+        MeetingRoomListView.ItemsSource = _reservationRequestManager.MeetingRooms;
+        RoomImage.Source = "hollowsquare_img.png";
+    }
+
+    private void RoomImageSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        selectedRoom = (MeetingRoom)MeetingRoomListView.SelectedItem;
+        RoomImage.Source = selectedRoom?.RoomImageFile;
+    }
+
+    private async void AddRequestButton_Clicked(object sender, EventArgs e)
+    {
+        MeetingRoom selectedRoom = (MeetingRoom)MeetingRoomListView.SelectedItem;
+
+        if (selectedRoom != null)
+        {
+            await Navigation.PushAsync(new AddRequestPage(selectedRoom, _reservationRequestManager));
+        }
+        else
+        {
+            await DisplayAlert("Alert", "Please select a room first.", "OK");
+        }
+    }
+
+    private async void ViewRequestButton_Clicked(object sender, EventArgs e)
+    {
+        if (selectedRoom != null)
+        {
+            await Navigation.PushAsync(new ViewRequestsPage(selectedRoom, _reservationRequestManager));
+        }
+        else
+        {
+            await DisplayAlert("Alert", "Please select a room first.", "OK");
+        }
+    }
 }
+
+
+
+
+
--- Assignment3/Pages/ViewRequestsPage.xaml.cs
+++ Assignment3/Pages/ViewRequestsPage.xaml.cs
@@ -1,9 +1,69 @@
+using Assignment3.BusinessLogic;
+using System.Collections.ObjectModel;
+
 namespace Assignment3.Pages;
 
 public partial class ViewRequestsPage : ContentPage
 {
-	public ViewRequestsPage()
-	{
-		InitializeComponent();
-	}
+    private MeetingRoom _selectedRoom;
+    private ReservationRequestManager _reservationRequestManager;
+    private ObservableCollection<ReservationRequest> _requestsForDisplay = new ObservableCollection<ReservationRequest>();
+
+    public ViewRequestsPage(MeetingRoom selectedRoom, ReservationRequestManager reservationRequestManager)
+    {
+        InitializeComponent();
+        _selectedRoom = selectedRoom;
+        _reservationRequestManager = reservationRequestManager;
+        PopulateRequestsForDisplay();
+        RequestsListView.ItemsSource = _requestsForDisplay;
+    }
+
+    private void PopulateRequestsForDisplay()
+    {
+        _requestsForDisplay.Clear();
+        foreach (var res in _reservationRequestManager.ReservationRequests)
+        {
+            if (res.MeetingRoom.RoomNumber == _selectedRoom.RoomNumber)
+            {
+                _requestsForDisplay.Add(res);
+            }
+        }
+    }
+
+    private void OnBackToRoomsClicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+
+    private void OnAcceptClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.BindingContext is ReservationRequest request)
+        {
+            try
+            {
+                _reservationRequestManager.AcceptReservationRequest(request);
+                PopulateRequestsForDisplay();
+                DisplayAlert("Success", "Request accepted successfully.", "OK");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+
+    private void OnRejectClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.BindingContext is ReservationRequest request)
+        {
+            _reservationRequestManager.ChangeReservationRequestStatus(request.RequestID, RequestStatus.Rejected);
+            PopulateRequestsForDisplay();
+            DisplayAlert("Success", "Request rejected successfully.", "OK");
+        }
+    }
 }
+
+
+
+
+
